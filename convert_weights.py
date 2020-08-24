92e1e20da9a096442566679bc71c5f37841ff1c4
Varun <varunasthana92@gmail.com>
2020-08-24 13:02:18 -0400

    experimental code added

--- convert_weights.py
+++ convert_weights.py
@@ -1,22 +1,26 @@
 #! /usr/bin/env python
 import numpy as np
-from yolov3 import YOLOv3Net
-from yolov3 import parse_cfg
+from yolo3 import *
+# from yolov3 import parse_cfg
+
 
 def load_weights(model, cfgfile, weightfile):
-	fp = open(weightfile, "rb")
+    fp = open(weightfile, "rb")
     # The first 5 values are header information
     # read and ignore
     np.fromfile(fp, dtype=np.int32, count=5)
     blocks = parse_cfg(cfgfile)
 
-	for i, block in enumerate(blocks[1:]):
+    for i, block in enumerate(blocks[1:]):
         if (block["type"] == "convolutional"):
             conv_layer = model.get_layer('conv_' + str(i))
             print("layer: ",i+1,conv_layer)
             filters = conv_layer.filters	# number of filteres or kernels
             k_size = conv_layer.kernel_size[0]
             in_dim = conv_layer.input_shape[-1]
+            norm_layer = None
+            bn_weights = None
+            conv_bias = None
             if "batch_normalize" in block:
                 norm_layer = model.get_layer('bnorm_' + str(i))
                 print("layer: ",i+1,norm_layer)
@@ -29,11 +33,11 @@ def load_weights(model, cfgfile, weightfile):
                 # tf batch_norm has [beta, gamma, mean, variance]
                 # hence need to do the [0 1 2 3] --> [1 0 2 3]
 
-            	# all attributes of wieghts are stacked for each kernel in rows
-            	# thus weights = [ [beta1, 	beta2, 	beta3, 	....beta_n	],
-            	#				   [gamm1, 	gamma2, gamma3,	....gamma_n	],
-            	# 				   [mean1, 	mean2,	mean3, 	....mean_n	],
-            	#				   [var1, 	var2, 	var3	....var_n	]]
+                # all attributes of wieghts are stacked for each kernel in rows
+                # thus weights = [ [beta1, 	beta2, 	beta3, 	....beta_n	],
+                #				   [gamm1, 	gamma2, gamma3,	....gamma_n	],
+                # 				   [mean1, 	mean2,	mean3, 	....mean_n	],
+                #				   [var1, 	var2, 	var3	....var_n	]]
                 bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
             else:
                 conv_bias = np.fromfile(fp, dtype=np.float32, count=filters)
@@ -49,9 +53,9 @@ def load_weights(model, cfgfile, weightfile):
             else:
                 conv_layer.set_weights([conv_weights, conv_bias])
 
-
-	assert len(fp.read()) == 0, 'failed to read all data'
-	fp.close()
+    assert len(fp.read()) == 0, 'failed to read all data'
+    fp.close()
+    return
 
 
 def main():
@@ -59,10 +63,19 @@ def main():
     cfgfile = "cfg/yolov3.cfg"
     model_size = (416, 416, 3)
     num_classes = 80
-    model=YOLOv3Net(cfgfile,model_size,num_classes)
-    load_weights(model,cfgfile,weightfile)
-    try:
-        model.save_weights('weights/yolov3_weights.tf')
-        print('\nThe file \'yolov3_weights.tf\' has been saved successfully.')
-    except IOError:
-        print("Couldn't write the file \'yolov3_weights.tf\'.")
+    # model=YOLOv3Net(cfgfile,model_size,num_classes)
+    imgPh = tf.placeholder(tf.float32, shape=(1, 416, 416, 3))
+    model = YOLOv3Net(cfgfile, imgPh, model_size, num_classes)
+    model.summary()
+    # model = YOLOv3Net_pre_trained(cfgfile, imgPh, model_size, num_classes, weightfile)
+    # load_weights(model,cfgfile,weightfile)
+    # try:
+    #     model.save_weights('weights/yolov3_weights.tf')
+    #     print('\nThe file \'yolov3_weights.tf\' has been saved successfully.')
+    # except IOError:
+    #     print("Couldn't write the file \'yolov3_weights.tf\'.")
+    # return
+
+
+if __name__ == '__main__':
+    main()
--- yolo3.py
+++ yolo3.py
@@ -28,15 +28,16 @@ def YOLOv3Net(cfgfile, imgPH, model_size, num_classes):
     filters = []
     out_pred = []
     scale = 0
-    # input_image = tf.keras.Input(shape=model_size)
-    # inputs = input_image
-    # inputs = inputs / 255.0
-    inputs = imgPH
+    input_image = tf.keras.Input(shape=model_size)
+    inputs = input_image
+    inputs = inputs / 255.0
+    # inputs = input_image=imgPH
 
     # as per the cgf file, block[0] is for [net] which contains the hyperparameters' values
     # hence iterating on block[1:]
     for i, block in enumerate(blocks[1:]):
-        if (block["type"] == "convolutional"):
+        print('Layer num i = ',i)
+        if block["type"] == "convolutional":
             activation = block["activation"]
             filters = int(block["filters"])
             kernel_size = int(block["size"])
@@ -45,28 +46,29 @@ def YOLOv3Net(cfgfile, imgPH, model_size, num_classes):
                 pad_layer = tf.keras.layers.ZeroPadding2D(((1, 0), (1, 0)))
                 inputs = pad_layer(inputs)
 
+            # in_shape = inputs.shape.as_list()
             conv2D_layer = tf.keras.layers.Conv2D(filters, kernel_size, strides=strides,
                             padding='valid' if strides > 1 else 'same', name='conv_' + str(i),
-                            use_bias=False if ("batch_normalize" in block) else True)
+                            use_bias=False if ("batch_normalize" in block) else True) # input_shape=in_shape)
             
             inputs = conv2D_layer(inputs)
             if "batch_normalize" in block:
                 inputs = tf.keras.layers.BatchNormalization(name='bnorm_' + str(i))(inputs)
                 inputs = tf.keras.layers.LeakyReLU(alpha=0.1, name='leaky_' + str(i))(inputs)
 
-        elif (block["type"] == "upsample"):
-                stride = int(block["stride"])
-                upsampling2D_layer = tf.keras.layers.UpSampling2D(stride)(inputs)
-                inputs = upsampling2D_layer(inputs)
+        elif block["type"] == "upsample":
+            stride = int(block["stride"])
+            upsampling2D_layer = tf.keras.layers.UpSampling2D(stride)
+            inputs = upsampling2D_layer(inputs)
 
-        '''
-         the attribute layers holds a value of -4 which means that if we are in route block,
-         we need to backward 4 layers and then output the feature map from that layer. However,
-         for the case of the route block whose attribute 'layers' in cfg file has 2 values like
-         in lines 633-634, layers contains -1 and 61, we need to concatenate the feature map
-         from a previous layer (-1) and the feature map from layer 61
-         '''
-        elif (block["type"] == "route"):
+            '''
+             the attribute layers holds a value of -4 which means that if we are in route block,
+             we need to backward 4 layers and then output the feature map from that layer. However,
+             for the case of the route block whose attribute 'layers' in cfg file has 2 values like
+             in lines 633-634, layers contains -1 and 61, we need to concatenate the feature map
+             from a previous layer (-1) and the feature map from layer 61
+             '''
+        elif block["type"] == "route":
             block["layers"] = block["layers"].split(',')
             start = int(block["layers"][0])
             if len(block["layers"]) > 1:
@@ -84,7 +86,6 @@ def YOLOv3Net(cfgfile, imgPH, model_size, num_classes):
             # "i+from" as we want to move from[0] backwards from current position and it is already negative
             # and add it to the previous layer feature map
             inputs = outputs[i - 1] + outputs[i + from_]
-
         elif block["type"] == "yolo":
             mask = block["mask"].split(",")
             mask = [int(x) for x in mask]
@@ -114,7 +115,6 @@ def YOLOv3Net(cfgfile, imgPH, model_size, num_classes):
             anchors = tf.tile(anchors, [out_shape[1] * out_shape[2], 1])
             box_shapes = tf.exp(box_shapes) * tf.cast(anchors, dtype=tf.float32)
 
-
             # convert relative positions of the center boxes into the real positions i.e. use the formulation
             # given by the author in the original paper for bx, by
             x = tf.range(out_shape[1], dtype=tf.float32)
@@ -125,22 +125,26 @@ def YOLOv3Net(cfgfile, imgPH, model_size, num_classes):
             cxy = tf.concat([cx, cy], axis=-1)
             cxy = tf.tile(cxy, [1, n_anchors])
             cxy = tf.reshape(cxy, [1, -1, 2])
-            strides = (input_image.shape[1] // out_shape[1], input_image.shape[2] // out_shape[2])
-            box_centers = (box_centers + cxy) * strides
+            in_shape = input_image.shape.as_list()
+            strides = (in_shape[1] / out_shape[1], in_shape[2] / out_shape[2])
+            print(input_image.shape)
+            print(out_shape[2])
+            print('strides = ', strides)
+            print('box_before shape', box_centers.shape)
+            box_centers = tf.multiply((box_centers + cxy), strides)
+            # print('box after shape', box_centers.shape)
             prediction = tf.concat([box_centers, box_shapes, confidence, classes], axis=-1)
             if scale:
                 out_pred = tf.concat([out_pred, prediction], axis=1)
             else:
                 out_pred = prediction
                 scale = 1
-
         outputs[i] = inputs
         output_filters.append(filters)
-
-    # model = Model(input_image, out_pred)
+    model = Model(input_image, out_pred)
     # model.summary()
-    # return model
-    return out_pred
+    return model
+    # return out_pred
 
 
 def YOLOv3Net_pre_trained(cfgfile, imgPH, model_size, num_classes, weightfile):
@@ -153,7 +157,7 @@ def YOLOv3Net_pre_trained(cfgfile, imgPH, model_size, num_classes, weightfile):
     # input_image = tf.keras.Input(shape=model_size)
     # inputs = input_image
     # inputs = inputs / 255.0
-    inputs = imgPH
+    inputs = input_image = imgPH
 
     fp = open(weightfile, "rb")
     # The first 5 values are header information
@@ -163,6 +167,7 @@ def YOLOv3Net_pre_trained(cfgfile, imgPH, model_size, num_classes, weightfile):
     # as per the cgf file, block[0] is for [net] which contains the hyperparameters' values
     # hence iterating on block[1:]
     for i, block in enumerate(blocks[1:]):
+        print('Layer num i = ', i)
         if (block["type"] == "convolutional"):
             activation = block["activation"]
             filters = int(block["filters"]) # number of filteres or kernels
@@ -172,14 +177,18 @@ def YOLOv3Net_pre_trained(cfgfile, imgPH, model_size, num_classes, weightfile):
                 pad_layer = tf.keras.layers.ZeroPadding2D(((1, 0), (1, 0)))
                 inputs = pad_layer(inputs)
 
+            in_shape = inputs.shape.as_list()
             conv2D_layer = tf.keras.layers.Conv2D(filters, kernel_size, strides=strides,
                             padding='valid' if strides > 1 else 'same', name='conv_' + str(i),
-                            use_bias=False if ("batch_normalize" in block) else True)
-            
+                            use_bias=False if ("batch_normalize" in block) else True, 
+                            input_shape=in_shape)
             # num_filters = conv2D_layer.filters    
             # k_size = conv2D_layer.kernel_size[0]
-            in_dim = conv2D_layer.input_shape[-1]
-
+            # in_dim = conv2D_layer.input_shape[-1]
+            in_dim = in_shape[-1]
+            norm_layer = None
+            bn_weights = None
+            conv_bias = None
             if "batch_normalize" in block:
                 norm_layer = tf.keras.layers.BatchNormalization(name='bnorm_' + str(i))
                 size = np.prod(norm_layer.get_weights()[0].shape)
@@ -209,22 +218,25 @@ def YOLOv3Net_pre_trained(cfgfile, imgPH, model_size, num_classes, weightfile):
                 inputs = conv2D_layer(inputs)
                 inputs = norm_layer(inputs)
                 inputs = tf.keras.layers.LeakyReLU(alpha=0.1, name='leaky_' + str(i))(inputs)
+                print("layer: ", i + 1, conv2D_layer)
+                print("layer: ", i + 1, norm_layer)
             else:
                 conv2D_layer.set_weights([conv_weights, conv_bias])
                 inputs = conv2D_layer(inputs)
+                print("layer: ", i + 1, conv2D_layer)
 
-        elif (block["type"] == "upsample"):
-                stride = int(block["stride"])
-                upsampling2D_layer = tf.keras.layers.UpSampling2D(stride)(inputs)
-                inputs = upsampling2D_layer(inputs)
+        elif block["type"] == "upsample":
+            stride = int(block["stride"])
+            upsampling2d_layer = tf.keras.layers.UpSampling2D(stride)(inputs)
+            inputs = upsampling2d_layer(inputs)
 
-        '''
-         the attribute layers holds a value of -4 which means that if we are in route block,
-         we need to backward 4 layers and then output the feature map from that layer. However,
-         for the case of the route block whose attribute 'layers' in cfg file has 2 values like
-         in lines 633-634, layers contains -1 and 61, we need to concatenate the feature map
-         from a previous layer (-1) and the feature map from layer 61
-         '''
+            '''
+             the attribute layers holds a value of -4 which means that if we are in route block,
+             we need to backward 4 layers and then output the feature map from that layer. However,
+             for the case of the route block whose attribute 'layers' in cfg file has 2 values like
+             in lines 633-634, layers contains -1 and 61, we need to concatenate the feature map
+             from a previous layer (-1) and the feature map from layer 61
+             '''
         elif (block["type"] == "route"):
             block["layers"] = block["layers"].split(',')
             start = int(block["layers"][0])
